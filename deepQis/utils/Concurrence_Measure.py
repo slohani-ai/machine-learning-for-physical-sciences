0360ea8e15279e0b038bcbf08dab27a42f539b78
slohani-ai <lohanisanjaya@gmail.com>
2022-01-15 20:47:02 -0600

    added first notebook

---
+++ deepQis/utils/Concurrence_Measure.py
@@ -0,0 +1,12 @@
+import numpy as np
+import qiskit.quantum_info as qi
+
+def concurrence_single(dm):
+    # dm = qi.DensityMatrix(dm)
+    con = qi.concurrence(dm)
+    return con
+
+def concurrence(dm_tensor):
+    con_list = list(map(concurrence_single, dm_tensor))
+    con_tensor = np.array(con_list)
+    return con_tensor
---
+++ deepQis/utils/Max_Eigen_Measure.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+def max_eigen(dm_tensor):
+    eigv, _ = tf.linalg.eigh(dm_tensor)
+    eigv = tf.math.real(eigv)
+    eigv_max = tf.reduce_max(eigv, axis=1)
+    return eigv_max
+
+def mean_eigen_order(dm_tensor):
+    eigv, _ = tf.linalg.eigh(dm_tensor)
+    eigv = tf.math.real(eigv)
+    eigv_mean = tf.reduce_mean(tf.sort(eigv, axis=1), axis=0)
+    return eigv_mean
