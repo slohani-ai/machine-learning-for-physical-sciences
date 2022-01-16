12378d6433e2c25c8072e684c846f0a378af6b2c
slohani-ai <lohanisanjaya@gmail.com>
2022-01-15 18:49:42 -0600

    simulator has been created.

--- deepQis/Biased_Distributions/utils/Rank_Measure.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-def rank(dm_tesnor):
-    ranks = tf.linalg.matrix_rank(dm_tesnor,tol=1e-5)
-    return ranks
---
+++ deepQis/Simulator/Measurements.py
@@ -0,0 +1,227 @@
+import _pickle as pkl
+import os
+
+import numpy as np
+import pandas as pd
+import qiskit.quantum_info as qi
+import tensorflow as tf
+import tensorflow_probability as tfp
+from utils import gen_basis_array as gen_proj
+
+
+
+class Ideal:
+
+    def __init__(self, qs=2):
+        self._qs = qs
+        if not os.path.exists(f'./utils/projectors_array_qs_{self.qs}.pickle'):
+            print('| To accelerate the simulation, Projectors file is created in utils folder.')
+            mqs = gen_proj.MultiQubitSystem(qubit_size=self._qs)
+            proj = mqs.IBM_Projectors()
+            with open(f'projectors_array_qs_{self._qs}.pickle', 'wb') as f:
+                pkl.dump(proj, f, -1)
+        else:
+            self.projectors = pd.read_pickle(f'./utils/projectors_array_qs_{self._qs}.pickle')
+
+    def get_tau_cholesky(self, density_matrix):
+        try:
+            chol = np.linalg.cholesky(density_matrix)
+        except:
+            dm = (1. - 0.0000001) * density_matrix + 0.0000001 / 4 * np.eye(2 ** self._qs)
+            chol = np.linalg.cholesky(dm)
+            pass
+        tau_first = np.real(chol.diagonal())  # len 16 for 4 qubits
+        ele_off_diags = chol[np.tril_indices(n=2 ** self._qs,
+                                             k=-1)]  # https://numpy.org/doc/stable/reference/generated/numpy.tril_indices.html
+        tau_second = []
+        for i in ele_off_diags:
+            re, img = np.real(i), np.imag(i)
+            tau_second.append(re)
+            tau_second.append(img)
+        tau = np.concatenate([tau_first, tau_second])
+        return tau
+
+    def get_tomo_from_density_matrix(self, density_matrix, projectors):
+        tomo = np.real(np.trace(np.matmul(density_matrix, projectors), axis1=1, axis2=2))
+        return tomo
+
+    def tomography_data(self, density_matrix, save_file=False, filename='pickle.pickle'):
+        if not isinstance(density_matrix, np.ndarray):
+            density_matrix = density_matrix.numpy()
+        tomo_list = list(map(lambda x: self.get_tomo_from_density_matrix(x, self.projectors), density_matrix))
+        tomo_array = np.array(tomo_list).reshape(-1, 6 ** self._qs)
+        tau_list = list(map(self.get_tau_cholesky, density_matrix))
+        tau_array = np.array(tau_list).reshape(-1, 2 ** self._qs * 2 ** self._qs)
+
+        if save_file:
+            if not os.path.exists('./data'):
+                os.mkdir('./data')
+                print('./data folder has been created and ')
+            with open(f'./data/{filename}', 'wb') as f:
+                print(f'tomography data has been saved into ./data/{filename}')
+                pkl.dump([tomo_array, tau_array, density_matrix], f, -1)
+
+        return [tomo_array, tau_array]
+
+class Random_Shots:
+
+    def __init__(self, qs=2, n_meas=1024):
+        self._qs = qs
+        self.n_shots = n_meas
+
+        if not os.path.exists(f'./utils/projectors_array_qs_{self.qs}.pickle'):
+            print('| To accelerate the simulation, Projectors file is created in utils folder.')
+            mqs = gen_proj.MultiQubitSystem(qubit_size=self._qs)
+            proj = mqs.IBM_Projectors()
+            with open(f'projectors_array_qs_{self._qs}.pickle', 'wb') as f:
+                pkl.dump(proj, f, -1)
+        else:
+            self.projectors = pd.read_pickle(f'./utils/projectors_array_qs_{self._qs}.pickle')
+
+        self.n_proj = self.projectors.reshape(3 ** self._qs, self._qs ** 2, self._qs ** 2, self._qs ** 2)
+        self.proj_used_rank_list = []
+
+    def get_tau_cholesky(self, density_matrix):
+        try:
+            chol = np.linalg.cholesky(density_matrix)
+        except:
+            dm = (1. - 0.0000001) * density_matrix + 0.0000001 / 4 * np.eye(2 ** self._qs)
+            chol = np.linalg.cholesky(dm)
+            pass
+        tau_first = np.real(chol.diagonal())  # len 16 for 4 qubits
+        ele_off_diags = chol[np.tril_indices(n=2 ** self._qs,
+                                             k=-1)]  # https://numpy.org/doc/stable/reference/generated/numpy.tril_indices.html
+        tau_second = []
+        for i in ele_off_diags:
+            re, img = np.real(i), np.imag(i)
+            tau_second.append(re)
+            tau_second.append(img)
+        tau = np.concatenate([tau_first, tau_second])
+        return tau
+
+    def get_tomo_from_density_matrix(self, projectors):
+        tomo = np.trace(np.real(np.matmul(self.rho, projectors))).astype(np.float32)  # one by one multiplication
+        return tomo
+
+    def measure_specific_basis(self, proj_int, dm_one):
+        self.rho = dm_one
+        # n_proj = self.projectors.reshape(3 ** self._qs, self._qs ** 2, self._qs ** 2, self._qs ** 2)
+        projectors = self.n_proj[proj_int]  # --> [4, 4, 4]
+        tomo_probs = list(map(self.get_tomo_from_density_matrix, projectors))
+        tomo_probs = np.array(tomo_probs).flatten()
+        draw = np.random.multinomial(1, tomo_probs)
+        arg_max_rank = np.argmax(draw)
+        measurements = np.zeros(6 ** self._qs).astype(np.float32)
+        measurements[4 * proj_int + arg_max_rank] = 1.
+        self.proj_used_rank_list.append(4 * proj_int + arg_max_rank)
+        return measurements
+
+    def measurement_array(self, dm=None):
+        dm = np.squeeze(dm)
+        proj_rank = np.random.randint(0, 9, self.n_shots)
+        measurement_list = list(map(lambda x: self.measure_specific_basis(x, dm), proj_rank))
+        measurement_array = np.array(measurement_list).reshape(-1, 6 ** self._qs)
+        self.count += 1
+        if self.count % 1000 == 0:
+            print(f'dm_finished: {self.count}')
+        return measurement_array
+
+    def tomography_data(self, density_matrix, norm=True, save_file=False, filename='pickle.pickle'):
+        self.count = 1
+        measurements = list(map(self.measurement_array, density_matrix))
+        measurements = np.array(measurements).reshape(-1, 9 * self.n_shots, 6 ** self._qs)
+        if norm:
+            tomo_array = np.sum(measurements, axis=1) / self.n_shots
+        else:
+            tomo_array = np.sum(measurements, axis=1)
+        tau_list = list(map(self.get_tau_cholesky, density_matrix))
+        tau_array = np.array(tau_list).reshape(-1, 2 ** self._qs * 2 ** self._qs)
+
+        if save_file:
+            if not os.path.exists('./data/data_shots'):
+                os.mkdir('./data/data_shots')
+                tf.print('./data/data_shots folder has been created and ')
+            with open(f'./data/data_shots/{filename}', 'wb') as f:
+                tf.print(f'tomography data has been saved into ./data/data_shots/{filename}')
+                pkl.dump([tomo_array, tau_array, density_matrix, self.proj_used_rank_list], f, -1)
+
+        # return measurements, self.proj_used_rank_list
+        return tomo_array, tau_array
+
+
+class NISQ_Shots:
+
+    def __init__(self, qs=2, shots=1024):
+        self._qs = qs
+        self.n_shots = shots
+
+        if not os.path.exists(f'./utils/projectors_array_qs_{self.qs}.pickle'):
+            print('| To accelerate the simulation, Projectors file is created in utils folder.')
+            mqs = gen_proj.MultiQubitSystem(qubit_size=self._qs)
+            proj = mqs.IBM_Projectors()
+            with open(f'projectors_array_qs_{self._qs}.pickle', 'wb') as f:
+                pkl.dump(proj, f, -1)
+        else:
+            self.projectors = pd.read_pickle(f'./utils/projectors_array_qs_{self._qs}.pickle')
+        # self.n_proj = self.projectors.reshape(3 ** self._qs, self._qs ** 2, self._qs ** 2, self._qs ** 2)
+        self.proj_used_rank_list = []
+
+    def get_tau_cholesky(self, density_matrix):
+        try:
+            chol = np.linalg.cholesky(density_matrix)
+        except:
+            dm = (1. - 0.0000001) * density_matrix + 0.0000001 / 4 * np.eye(2 ** self._qs)
+            chol = np.linalg.cholesky(dm)
+            pass
+        tau_first = np.real(chol.diagonal())  # len 16 for 4 qubits
+        ele_off_diags = chol[np.tril_indices(n=2 ** self._qs,
+                                             k=-1)]  # https://numpy.org/doc/stable/reference/generated/numpy.tril_indices.html
+        tau_second = []
+        for i in ele_off_diags:
+            re, img = np.real(i), np.imag(i)
+            tau_second.append(re)
+            tau_second.append(img)
+        tau = np.concatenate([tau_first, tau_second])
+        return tau
+
+    def get_tomo_from_density_matrix(self, rho, projectors):  # --> Ideal measurements
+        tomo = np.trace(np.real(np.matmul(rho, projectors)), axis1=1, axis2=2).astype(
+            np.float32)  # multiplication with broadcasting the arrays
+        return tomo
+
+    def draw_multinomial(self, row_prob):
+        return np.random.multinomial(self.n_shots, row_prob)
+
+    def measurement_array(self, dm=None):
+        n_proj = self.projectors.reshape(-1, 4, 4)
+        dm = dm.reshape(1, 4, 4)
+        ideal_tomo_list = self.get_tomo_from_density_matrix(dm, n_proj)
+        ideal_tomo_array = np.array(ideal_tomo_list).reshape(-1, 2 ** self._qs)
+        measurement_list = list(map(self.draw_multinomial, ideal_tomo_array))
+        measurement_array = np.array(measurement_list).flatten()
+        self.count += 1
+        if self.count % 1000 == 0:
+            print(f'dm_finished: {self.count}')
+        return measurement_array
+
+    def tomography_data(self, density_matrix, norm=True, save_file=False, filename='pickle.pickle'):
+        self.count = 1
+        measurements = list(map(self.measurement_array, density_matrix))
+        measurements = np.array(measurements).reshape(-1, 6 ** self._qs)
+        if norm:
+            tomo_array = measurements / self.n_shots
+        else:
+            tomo_array = measurements
+        tau_list = list(map(self.get_tau_cholesky, density_matrix))
+        tau_array = np.array(tau_list).reshape(-1, 2 ** self._qs * 2 ** self._qs)
+
+        if save_file:
+            if not os.path.exists('./data/data_shots'):
+                os.mkdir('./data/data_shots')
+                tf.print('./data/data_shots folder has been created and ')
+            with open(f'./data/data_shots/{filename}', 'wb') as f:
+                tf.print(f'tomography data has been saved into ./data/data_shots/{filename}')
+                pkl.dump([tomo_array, tau_array, density_matrix, self.proj_used_rank_list], f, -1)
+
+        # return measurements, self.proj_used_rank_list
+        return tomo_array, tau_array
