2b07aca8e0f0af992b685f8b4909ba069f982529
slohani-ai <lohanisanjaya@gmail.com>
2022-01-29 01:28:41 -0600

    setup

--- deepqis/network/inference.py
+++ deepqis/network/inference.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 import pkg_resources
 from deepqis.utils import Extract_Net as arlnet
+import os
 
 def load():
     """ Return pre-trained model"""
-    stream = pkg_resources.resource_stream(__name__, '/deepqis/models/ARL_ORNL_meas_n_1000_qubits_2_batch_4_alpha_0.4_BEST.h5')
-    return stream
+    path_to_model_, file_name = os.path.split(__file__)
+    path_to_model = os.path.join(path_to_model_, 'models/ARL_ONRL_meas_n_1000_qubits_2_batch_4_alpha_0.4_BEST.h5')
+    # stream = pkg_resources.resource_stream(path_to_model)
+    return path_to_model
 
 def fit(input_data):
 
@@ -18,8 +21,8 @@ def fit(input_data):
         std = tf.math.reduce_std(row_matrix)
         scaled = (row_matrix - m) / std
         return scaled
-
-    x_in = scaling_mean_0_std_1(input_data, [-1, 6, 6, 1])
+    x_test = tf.vectorized_map(scaling_mean_0_std_1, input_data)
+    x_in = tf.reshape(x_test, [-1, 6, 6, 1])
     logits, dm_pred = model(x_in)
 
     return dm_pred
